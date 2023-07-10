365807c4a78437aa003d95495617529c3545a1ed
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2023-07-11 01:35:39 +0800

    Added sign out logic

--- lib/state/helpers/auth_helpers.dart
+++ lib/state/helpers/auth_helpers.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 Future<bool> checkIfUserExists(String userId) async {
   final users = FirebaseFirestore.instance.collection('users');
   final snapshot = await users.where('userId', isEqualTo: userId.toString()).get();
 
   return snapshot.docs.isEmpty;
+}
+
+Future<void> signOut() async {
+  await GoogleSignIn().signOut();
+  await FirebaseAuth.instance.signOut();
 }
